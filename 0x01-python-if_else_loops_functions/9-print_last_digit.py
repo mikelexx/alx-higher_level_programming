30bcf436f35c7b607e51b22a7797253e06ab8766
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-01-04 08:08:29 +0300

    removed multiple spaces after operator

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,8 +2,8 @@
 def print_last_digit(number):
     last = 0
     if number < 0:
-        last =  (0 - number) % 10
+        last = (0 - number) % 10
     else:
-        last =  number % 10
+        last = number % 10
     print(last, end='')
     return last
