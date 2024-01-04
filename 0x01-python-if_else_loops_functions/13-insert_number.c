a9f9253d3a373768e730b94a1ceb2d8e725e98d0
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-01-04 09:20:19 +0300

    added case for single node with greater value tthan argument number

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -10,9 +10,9 @@ listint_t *insert_node(listint_t **head, int number)
 		return (NULL);
 	newnode->n = number;
 	newnode->next = NULL;
-	if (curr->next == NULL)
+	if (curr->n > number)
 	{
-		curr->next = newnode;
+		newnode->next =  curr;
 		return (newnode);
 	}
 	while (curr != NULL)
