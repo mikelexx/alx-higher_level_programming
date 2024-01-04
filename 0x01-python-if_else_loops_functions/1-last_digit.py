48b5ac42615c20f830cb0af59243397e703b8f4e
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-01-04 04:48:02 +0300

    fixed printing absolute of a last digit less than 0

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+last = number % 10
 if number < 0:
-    last = (0 - number) % 10
-else:
-     last = number % 10
+    last = 0 - ((0 - number) % 10)
 print("Last digit of", number, "is", last, end="")
 if last > 5 and number > 0:
     print(" and is greater than 5")
-elif last == 0 and number > 0:
+elif last == 0 and number >= 0:
     print(" and is 0")
 else:
     print(" and is less than 6 and not 0")
