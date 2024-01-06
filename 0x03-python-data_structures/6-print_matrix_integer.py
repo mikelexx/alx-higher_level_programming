045908562dede7ab216c52ad2b626631e144578c
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-01-06 19:36:24 +0300

    added :d specifier to print string format

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,8 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     for list in matrix:
         for n in list[:-1]:
-            print("{}".format(n), end=" ")
+            print("{:d}".format(n), end=" ")
         if list:
-            print("{}".format(list[-1]))
+            print("{:d}".format(list[-1]))
         else:
             print()
