469d43b324f6a37657b4014dfac145bcfadf04bf
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-01-10 15:06:29 +0300

    changed to using dict.copy()

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dict = {}
-    for key, value in a_dictionary.items():
-        new_dict[key] = value * 2
+    new_dict = a_dictionary.copy()
+    for key in new_dict:
+        new_dict[key] *= 2
     return new_dict
