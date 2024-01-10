540385dfed3eb7f4b95d9b9501ebac7e4fba2cbd
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-01-10 14:27:50 +0300

    added check for non-string argument to return None

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string:
+    if not roman_string or roman_string in [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]:
         return None
-    romans = {"I":1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M":1000}
+    romans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     prev = roman_string[0]
-    count =  0
+    count = 0
     for n in roman_string:
         if n not in romans:
             return None
