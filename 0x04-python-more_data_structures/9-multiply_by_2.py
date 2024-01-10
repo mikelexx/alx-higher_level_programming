abf652e82bd0414464486b87e70e53c1c19e5d9b
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-01-10 15:10:13 +0300

    use list comprehension for clean and minimal code

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dict = a_dictionary.copy()
-    for key in new_dict:
-        new_dict[key] *= 2
-    return new_dict
+    return {key: a_dictionary[key] * 2 for key in a_dictionary.keys()}
