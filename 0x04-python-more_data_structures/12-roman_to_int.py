26d5d5caa4b073876c1c3e0f41477301a9d0c574
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-01-10 14:38:33 +0300

    changed to using isinstance to check if type of argument is string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or roman_string in [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]:
+    if not roman_string or not isinstance(roman_string, str):
         return 0
     romans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     prev = roman_string[0]
     count = 0
+    freq = 0
     for n in roman_string:
         if n not in romans:
             return 0
         if romans[n] > romans[prev]:
             count -= romans[prev]
             count -= romans[prev]
+        if n == prev:
+            freq += 1
         count += romans[n]
         prev = n
     return count
