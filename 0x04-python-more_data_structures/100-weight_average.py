9c5ec45c5b03ab8a2460ad002034e1051ea044bc
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-01-10 15:44:20 +0300

    Weighted average!

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+    total_score = 0
+    total_weight = 0
+    for tup in my_list:
+        score, weight = tup
+        total_score += (score * weight)
+        total_weight += weight
+    return total_score / total_weight
+            
