77ad51f84fc33992b34fe88adfbcd39a0dc697f3
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-01-22 14:03:33 +0300

    [fx]: raise IndexError for uniqual sizes of array before any operation on them

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,14 +3,13 @@ def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(max(len(my_list_1), len(my_list_2))):
         try:
-            first = float(my_list_1[i])
-            second = float(my_list_2[i])
+            if i >= len(my_list_1) or i >= len(my_list_2):
+                raise IndexError
+            first = my_list_1[i]
+            second = my_list_2[i]
             res = first / second
         except IndexError:
             print("out of range")
-            res = 0
-        except ValueError:
-            print("wrong type")
             res = 0
         except ZeroDivisionError:
             print("division by 0")
