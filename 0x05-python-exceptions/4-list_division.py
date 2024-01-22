4fd044452b48fc11ce47e23f580d19754db62de9
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-01-22 14:39:16 +0300

    removed raising index error myself

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,8 +3,6 @@ def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(list_length):
         try:
-            if i >= len(my_list_1) or i >= len(my_list_2):
-                raise IndexError
             first = my_list_1[i]
             second = my_list_2[i]
             res = first / second
