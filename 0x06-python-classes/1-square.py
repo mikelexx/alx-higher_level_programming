d779f9599429777fa97b0244267efc166c73e1a0
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-01-23 16:23:41 +0300

    added args section in class documtention

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -10,6 +10,9 @@ Contains class that defines a square shape.
 class Square:
     """
     Class Square that defines a square by a private instance attribute: size.
+    Args:
+        size: to be used as dimensoin of square.
+
     """
     def __init__(self, size):
         self.__size = size
