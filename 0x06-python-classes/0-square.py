08beedf053c3b07f16f201826353802e1c775df2
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-01-23 15:29:09 +0300

    added more documetion of class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -4,8 +4,9 @@ This module contains an empty class that defines a square.
 """
 
 class Square:
-    """This is a class that defines the square shape.
-        This class foes nothing for now and has no attributes. 
+    """ This is a class that defines the square shape.
+        This class foes nothing for now and has no attributes but the docs.
+        Says i should add more lines so i have no other option>
     """
 
     pass
