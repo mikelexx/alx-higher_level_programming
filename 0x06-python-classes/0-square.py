9d6a24c0d8f9f2ed3204da2c13adde89185b991f
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-01-23 15:24:21 +0300

    added space between documentation and class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,9 +2,11 @@
 """ 0-square.py
 This module contains an empty class that defines a square.
 """
+
 class Square:
     """
     This is a class that defines the square shape but for now it has 
-    no attributes
+    no attributes.
     """
+
     pass
