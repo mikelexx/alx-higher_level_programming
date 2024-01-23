abc52cf7266eb22f2f6c9f8e631f47f953f5d8a9
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-01-23 15:32:36 +0300

    added two black lines before class defination

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -3,10 +3,12 @@
 This module contains an empty class that defines a square.
 """
 
+
 class Square:
     """ This is a class that defines the square shape.
+
         This class foes nothing for now and has no attributes but the docs.
-        Says i should add more lines so i have no other option>
+
     """
 
     pass
