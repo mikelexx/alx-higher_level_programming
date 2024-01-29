618fa8aabb4a4c5a4a35516fda987c9715717aa7
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-01-29 20:29:33 +0300

    added more content for docstring to met required minimal number of lines

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
 This module contains an empty class that defines a rectangle.
-
+The Rectangle class for now has no attributes or methods.
+The Rectangle does nothing.
 """
 
 
