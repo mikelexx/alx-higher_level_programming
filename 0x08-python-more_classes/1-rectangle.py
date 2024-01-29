30960f8d6d63a687b9138dddbf9510f193a85766
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-01-29 21:09:30 +0300

    rectify if conditions to allow assignment of arg to instance if it passes checks for validity

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -18,14 +18,16 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
-        if width < 0:
+        elif width < 0:
             raise ValueError("width must be >= 0")
+        else:
+            self.__width = width
         if not isinstance(height, int):
             raise TypeError("height must be an integer")
-        if height < 0:
+        elif height < 0:
             raise ValueError("height must be >= 0")
-        self.__width = width
-        self.__height = height
+        else:
+            self.__height = height
 
     @property
     def width(self):
