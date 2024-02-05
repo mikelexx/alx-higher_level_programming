2438d197bff66f0942e100d26503b76545b99a27
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-02-05 18:49:59 +0300

    6.Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+contains class based on module 5-base_geometry
+"""
+
+
+class BaseGeometry:
+
+    def area(self):
+        """
+        this function  does nothing
+        Raises:
+            Exception
+        """
+        raise Exception("area() is not implemented")
