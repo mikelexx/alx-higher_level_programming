467135d5b3de2612eec06eab4a00fa7a1f19b761
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-02-05 18:55:06 +0300

    add documentation for class

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -5,6 +5,9 @@ contains class based on module 5-base_geometry
 
 
 class BaseGeometry():
+    """
+    this class contains a function that is not implemented.
+    """
 
     def area(self):
         """
