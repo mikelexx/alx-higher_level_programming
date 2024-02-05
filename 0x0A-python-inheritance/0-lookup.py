570abc1305a871d93a12408750e5f76ab905c172
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-02-05 16:37:36 +0300

    fix pycodestyle error -too many characters on a line

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """
-this modules has a function that returns the list of available attributes and methods of an object.
+this modules has a function that returns \
+        the list of available attributes and methods of an object.
 """
+
+
 def lookup(obj):
     """
     returns the list of available attributes and methods of an object.
