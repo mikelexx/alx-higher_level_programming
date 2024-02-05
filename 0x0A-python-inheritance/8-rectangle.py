cbaae387cb8b4740087b29253c06479ef627f47d
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-02-05 21:02:12 +0300

    8. Rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+this module defines a class Rectangle that\
+        inherits from BaseGeometry (7-base_geometry.py).
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    this class defines rectangle shape.
+    Args:
+        width (int): width of rectangle
+        height (int): height of rectangle
+    Raises:
+        e : for any error caught in in subsequent calls.
+    """
+    def __init__(self, width, height):
+        try:
+            self.integer_validator("width", width)
+            self.integer_validator("height", height)
+        except ValueError as e:
+            raise e
+        except TypeError as e:
+            raise e
+        else:
+            self.__width = width
+            self.___height = height
