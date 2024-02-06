f7d8737427f3733be54599e12cd6fe6943dd7612
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-02-06 20:59:24 +0300

    remove unnecessary printing in function

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,13 +4,17 @@ this script adds all arguments to a Python list, and then\
         save them to a file:
 """
 import sys
-import sys
+import json
 
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-my_list = sys.argv[1:]
-data = []
-for arg in my_list:
-    data.append(arg)
-save_to_json_file(data, "add_item.json")
+obj = []
+try:
+    obj = load_from_json_file("add_item.json")
+except: 
+        pass
+for arg in sys.argv[1:]:
+    obj.append(arg)
+save_to_json_file(obj, "add_item.json")
+    
