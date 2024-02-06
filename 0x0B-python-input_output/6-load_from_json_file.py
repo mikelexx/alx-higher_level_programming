42fea088f0c78a18e96d4d7cdd65abdff19c2225
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-02-06 16:59:37 +0300

    add documentation for function

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,6 +6,13 @@ import json
 
 
 def load_from_json_file(filename):
+    """
+    creates an Object from a “JSON file”:
+    Args:
+        filename: Json file containing JSON representation of object.
+    Returns:
+        object: object from its JSON representation.
+    """
     with open(filename, 'r', encoding="UTF8") as f:
         data = json.loads(f.read())
         return data
