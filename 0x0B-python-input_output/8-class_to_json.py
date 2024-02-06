022bd902411633079375bcc9a8143bf712f6e2e0
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-02-06 21:28:08 +0300

    remove any import statement as it's prohibited for this task

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -13,8 +13,11 @@ def class_to_json(obj):
     serializes the attributes and methods of an object.
     Args:
         obj: an object of any type
-    returns: the dictionary description with \
+    Returns: the dictionary description with \
 simple data structure (list, dictionary, string, \
 integer and boolean) for JSON serialization of an object:
     """
-    return json.dumps(obj.__dict__)
+    js = {}
+    for key, val in obj.__dict__.items():
+        js[key] = val
+    return js
