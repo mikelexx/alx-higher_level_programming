79faba787283517fb1e995d43c571bebfb1cd66d
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-03-12 18:55:27 +0300

    18. Temperatures #0

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,13 @@
+-- Import in hbtn_0c_0 database this table dump: temperatures.sql
+-- 
+-- Write a script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
+USE hbtn_0c_0
+CREATE TABLE IF NOT EXISTS temperature
+(city VARCHAR(15), state VARCHAR (15), year INT, month INT, temp INT);
+LOAD DATA
+LOCAL INFILE 'temperature'
+INTO TABLE temperature;
+SELECT city, AVG(temp) as avg_temp
+FROM temperature
+GROUP BY city
+ORDER BY avg_temp;
