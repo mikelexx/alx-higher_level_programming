48e2446b0bedfec7b729f87e1e80edfd3c84c445
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-03-13 19:00:57 +0300

    User user_0d_2@localhost should have only SELECT ON grant on the datase tables

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -5,8 +5,8 @@
 -- If the database hbtn_0d_2 already exists, your script should not fail
 -- If the user user_0d_2 already exists, your script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
 IDENTIFIED BY 'user_0d_2_pwd';
-GRANT ALL PRIVILEGES
-ON *.*
-TO 'user_0d_2';
+GRANT SELECT
+ON hbtn_0d_2.*
+TO 'user_0d_2'@'localhost';
