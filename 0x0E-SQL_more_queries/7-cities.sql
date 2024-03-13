be93b5de5fae64f25ca4727112f30bda73666d62
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-03-13 21:38:41 +0300

    add to which database is the referenced table from in foregn key

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -9,8 +9,6 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(256) NOT NULL,
-	state_id INT NOT NULL, 
-	CONSTRAINT FOREIGN KEY 
-	(state_id)
-	REFERENCES states (id)
+	state_id INT NOT NULL,
+	FOREIGN KEY (states_id) REFERENCES hbtn_0d_usa.states (id)
 	);
