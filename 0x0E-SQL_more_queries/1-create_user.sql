4f795396835d7519154bfa8047640bada02ea919
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-03-13 16:53:54 +0300

    remove auth_socke, its a must for user to enter password before login into database

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -4,4 +4,4 @@
 -- The user_0d_1 password should be set to user_0d_1_pwd
 -- If the user user_0d_1 already exists, your script should not fail
 CREATE USER IF NOT EXISTS 'user_0d_1' 
-IDENTIFIED WITH auth_socket BY 'user_0d_1_pwd';
+IDENTIFIED WITH BY 'user_0d_1_pwd';
