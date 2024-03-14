fbcaa1907cd3b6084f005183bc03ddb1194868e1
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-03-14 05:15:49 +0300

    City should be created inside hbtn_0d_usa database

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,9 +6,9 @@
 -- name VARCHAR(256) can’t be null
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
 	id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(256) NOT NULL,
 	state_id INT NOT NULL,
-	FOREIGN KEY (states_id) REFERENCES hbtn_0d_usa.states (id)
+	FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states (id)
 	);
