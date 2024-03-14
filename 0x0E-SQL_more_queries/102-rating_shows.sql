009494bb5c407ea506a7d87c33babb18bfd71dc2
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-03-14 16:03:47 +0300

    Display proper column name in the result

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -4,9 +4,9 @@
 -- Results must be sorted in descending order by the rating
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS "rating sum"
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows
 JOIN tv_show_ratings
 ON tv_shows.id  = tv_show_ratings.show_id
 GROUP BY tv_shows.id
-ORDER BY `rating sum` DESC;
+ORDER BY rating DESC;
