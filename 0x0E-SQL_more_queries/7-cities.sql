5d48f4928cea156ed837d2f9e3f796e8b06b47de
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-03-14 05:21:08 +0300

    correct the order of fields in creating tables

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,9 +5,10 @@
 -- state_id INT, can’t be null and must be a FOREIGN KEY that references to id of the states table
 -- name VARCHAR(256) can’t be null
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
 	id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(256) NOT NULL,
 	state_id INT NOT NULL,
-	FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states (id)
+	name VARCHAR(256) NOT NULL,
+	FOREIGN KEY (state_id) REFERENCES states (id)
 	);
