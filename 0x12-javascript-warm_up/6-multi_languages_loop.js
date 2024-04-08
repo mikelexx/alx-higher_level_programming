cc73ddb813cc344af633cd8570a2bee8d928decc
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-04-08 19:38:37 +0300

    6. Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const statements = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let count = 0;
+while (count < statements.length) {
+  console.log(statements[count]);
+  count++;
+}
