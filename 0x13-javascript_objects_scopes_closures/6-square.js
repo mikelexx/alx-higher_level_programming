839d91693f393bba4b8c75d437b0393671e9ce35
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-04-09 22:35:41 +0300

    fix the semistandard rules errors

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,16 +2,16 @@
 const ParentSquare = require('./5-square');
 module.exports = class Square extends ParentSquare {
   charPrint (c) {
-	  let delimiter = c;
-	  if (typeof delimiter === 'undefined') {
-		  delimiter = 'X';
-	  }
-	  let tmp = delimiter;
-	  for (let count = 1; count < this.width; count++) {
-		  tmp = tmp.concat(delimiter);
-	  }
-	  for (let count = 0; count < this.height; count++) {
-		  console.log(tmp);
-	  }
+    let delimiter = c;
+    if (typeof delimiter === 'undefined') {
+      delimiter = 'X';
+    }
+    let tmp = delimiter;
+    for (let count = 1; count < this.width; count++) {
+      tmp = tmp.concat(delimiter);
+    }
+    for (let count = 0; count < this.height; count++) {
+      console.log(tmp);
+    }
   }
 };
