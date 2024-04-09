2a9213eca34083c7753650f8b6d3b281aa7ba8fc
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-04-09 20:03:00 +0300

    3. Rectangle #3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && (!isNaN(w)) && w > 0 && typeof h === 'number' && (!isNaN(h)) && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print = () => {
+    let tmp = 'X';
+    for (let counter = 1; counter < this.width; counter++) {
+      tmp = tmp.concat('X');
+    }
+    for (let counter = 0; counter < this.height; counter++) {
+      console.log(tmp);
+    }
+  };
+};
