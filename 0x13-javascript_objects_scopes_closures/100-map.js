432d5b513492810ae9046607ad3e602bdff26016
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-04-10 00:27:22 +0300

    delete new array decrataion as map already returns new array

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-const res = [];
-list.map((el) => res.push(el * list.indexOf(el)));
+const res = list.map(el => el * list.indexOf(el));
 console.log(list);
 console.log(res);
