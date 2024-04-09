7d1afe18ea03ccea1509b8588f6c46dffc171fa0
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-04-09 19:47:22 +0300

    Fix semistandard errors

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-	  if (typeof w === 'number' && (!isNaN(w)) && w > 0 && typeof h === 'number' && (!isNaN(h)) && h > 0) {
-		  this.width = w;
-		  this.height = h;
-	  }
+    if (typeof w === 'number' && (!isNaN(w)) && w > 0 && typeof h === 'number' && (!isNaN(h)) && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
   }
 };
