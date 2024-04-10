ff80df339d51b41bb5673c33020b39c010e04253
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-04-10 17:03:05 +0300

    fixed incorrect identations of code

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,9 +9,10 @@ database_name = sys.argv[3]
 db = MySQLdb.connect(host="localhost",
                      user=username, passwd=password, db=database_name)
 cur = db.cursor()
-row_count = cur.execute("SELECT cities.id, cities.name, states.name
-                        FROM cities LEFT JOIN
-                        states ON cities.state_id=states.id
-                        ORDER BY cities.id")
+row_count = cur.execute(
+        ("SELECT cities.id, cities.name, states.name"
+            " FROM cities LEFT JOIN"
+            " states ON cities.state_id=states.id"
+            " ORDER BY cities.id"))
 for entry in cur.fetchall():
     print(entry)
