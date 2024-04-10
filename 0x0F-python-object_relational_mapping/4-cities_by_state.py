46100d41ba1a96a4554a72c76d439e7d170eb3a9
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-04-10 16:30:44 +0300

    4. Cities by states

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ != "__main__":
+    exit()
+import sys
+import MySQLdb
+username = sys.argv[1]
+password = sys.argv[2]
+database_name = sys.argv[3]
+db = MySQLdb.connect(host="localhost",
+                     user=username, passwd=password, db=database_name)
+cur = db.cursor()
+row_count = cur.execute("SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON cities.state_id = states.id ORDER BY cities.id")
+for entry in cur.fetchall():
+        print(entry)
