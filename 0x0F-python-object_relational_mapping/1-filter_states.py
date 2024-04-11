f6c3db52e925a125144239b21dd6e8e015e3b61b
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-04-11 07:06:58 +0300

    code must not execute when imported

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,16 +4,15 @@ Write a script that lists all states with a name starting \
 with N (upper N) from the database hbtn_0e_0_usa:
 """
 if __name__ != "__main__":
-    exit()
-import sys
-import MySQLdb
-username = sys.argv[1]
-password = sys.argv[2]
-database_name = sys.argv[3]
-db = MySQLdb.connect(host="localhost",
-                     user=username, passwd=password, db=database_name)
-cur = db.cursor()
-row_count = cur.execute("SELECT * FROM states ORDER BY id")
-for entry in cur.fetchall():
-    if entry[1][0] == "N":
-        print(entry)
+    import sys
+    import MySQLdb
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    db = MySQLdb.connect(host="localhost",
+                         user=username, passwd=password, db=database_name)
+    cur = db.cursor()
+    row_count = cur.execute("SELECT * FROM states ORDER BY id")
+    for entry in cur.fetchall():
+        if entry[1][0] == "N":
+            print(entry)
