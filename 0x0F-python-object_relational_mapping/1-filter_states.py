c917bdd6033977285311e1e042f68d18b0e7be63
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-04-11 07:08:10 +0300

    import statements should be before any code

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,9 +3,9 @@
 Write a script that lists all states with a name starting \
 with N (upper N) from the database hbtn_0e_0_usa:
 """
+import sys
+import MySQLdb
 if __name__ != "__main__":
-    import sys
-    import MySQLdb
     username = sys.argv[1]
     password = sys.argv[2]
     database_name = sys.argv[3]
