44acb0d4583686718a1e321e3973a116682f7c7a
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-04-11 09:59:03 +0300

    9. Contains

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -3,17 +3,16 @@
 THis module enables to talking to the database using Session.
 The ORM’s “handle” to the database is the Session.
 """
-if __name__ != "__main__":
-    exit()
-from sqlalchemy import create_engine
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
-import sys
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                       .format(sys.argv[1], sys.argv[2],
-                               sys.argv[3]), pool_pre_ping=True)
-Session = sessionmaker(bind=engine)
-session = Session()
-states = session.query(State).order_by(State.id).filter("a" in State.name)
-for state in states:
-    print("{}: {}".format(state.id, state.name))
+if __name__ == "__main__":
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    import sys
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).order_by(State.id):
+        if "a" in state.name:
+            print("{}: {}".format(state.id, state.name))
