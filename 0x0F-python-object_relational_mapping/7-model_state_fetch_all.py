337aa1c66d4e7bf465985438d996852001004dfc
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-04-11 09:02:03 +0300

    code must be executed only when not imported

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,16 +3,15 @@
 THis module enables to talking to the database using Session.
 The ORM’s “handle” to the database is the Session.
 """
-if __name__ != "__main__":
-    exit()
-from sqlalchemy import create_engine
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
-import sys
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                       .format(sys.argv[1], sys.argv[2],
-                               sys.argv[3]), pool_pre_ping=True)
-Session = sessionmaker(bind=engine)
-session = Session()
-for state in session.query(State).order_by(State.id):
-    print("{}: {}".format(state.id, state.name))
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+    import sys
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
