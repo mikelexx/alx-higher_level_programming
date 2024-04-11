abcc5123071b285e32bb6f8712bd281fd31dbc3b
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-04-11 07:30:18 +0300

    code must not be executed when imported

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,17 +5,16 @@ all values in the states table of hbtn_0e_0_usa where\
 name matches the argument.
 """
 if __name__ != "__main__":
-    exit()
-import sys
-import MySQLdb
-username = sys.argv[1]
-password = sys.argv[2]
-database_name = sys.argv[3]
-match_string = sys.argv[4]
-db = MySQLdb.connect(host="localhost",
-                     user=username, passwd=password, db=database_name)
-cur = db.cursor()
-table = cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id",
-                    (match_string,))
-for row in cur.fetchall():
-    print(row)
+    import sys
+    import MySQLdb
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    match_string = sys.argv[4]
+    db = MySQLdb.connect(host="localhost",
+                         user=username, passwd=password, db=database_name)
+    cur = db.cursor()
+    table = cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id",
+                        (match_string,))
+    for row in cur.fetchall():
+        print(row)
