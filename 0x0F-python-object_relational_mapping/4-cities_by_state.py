b50e0582673847cc7c109b86ce8ae6f46854c798
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-04-11 08:25:27 +0300

    code must not be executed when imported

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -2,20 +2,18 @@
 """
 Write a script that lists all cities from the database hbtn_0e_4_usa
 """
-if __name__ != "__main__":
-    exit()
-import sys
-import MySQLdb
-username = sys.argv[1]
-password = sys.argv[2]
-database_name = sys.argv[3]
-db = MySQLdb.connect(host="localhost",
-                     user=username, passwd=password, db=database_name)
-cur = db.cursor()
-row_count = cur.execute(
-        ("SELECT cities.id, cities.name, states.name"
-            " FROM cities LEFT JOIN"
-            " states ON cities.state_id=states.id"
-            " ORDER BY cities.id"))
-for entry in cur.fetchall():
-    print(entry)
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    db = MySQLdb.connect(host="localhost",
+                         user=username, passwd=password, db=database_name)
+    cur = db.cursor()
+    row_count = cur.execute("""SELECT cities.id, cities.name, states.name
+                FROM cities LEFT JOIN
+                states ON cities.state_id=states.id
+                ORDER BY cities.id""")
+    for entry in cur.fetchall():
+        print(entry)
