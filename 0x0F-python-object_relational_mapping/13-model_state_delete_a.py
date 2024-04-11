fafe8af63c6a659197a02219492dd2feeec51e35
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-04-11 12:41:51 +0300

    add docstring

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+script that deletes all State objects with \
+    a name containing the letter a from the database hbtn_0e_6_usa
+"""
 if __name__ == "__main__":
     from model_state import Base, State
     from sqlalchemy import create_engine
