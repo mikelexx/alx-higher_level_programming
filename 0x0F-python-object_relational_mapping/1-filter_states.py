4b94a6ca99650d18364c05d0f03aa5fde9cd4b4e
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-04-11 07:21:15 +0300

    fix pycode style and use format inside sql squery for pattern matching

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,9 +10,11 @@ if __name__ == "__main__":
     password = sys.argv[2]
     database_name = sys.argv[3]
     db = MySQLdb.connect(host="localhost",
-                         user=username, passwd=password, db=database_name)
+                         user=username,
+                         passwd=password, db=database_name)
     cur = db.cursor()
-    row_count = cur.execute("SELECT * FROM states ORDER BY id")
+    row_count = cur.execute(
+            ("SELECT * FROM states WHERE "
+             "name LIKE 'N%' ORDER BY id"))
     for entry in cur.fetchall():
-        if entry[1][0] == "N":
-            print(entry)
+        print(entry)
