39d68aa8951801e41cd3b31ff036025fff17e24a
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-04-27 05:42:57 +0300

    remove extra words in print statement

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     import requests
     import sys
     r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
-    print('Your email is: {}'.format(r.text))
+    print(r.text)
