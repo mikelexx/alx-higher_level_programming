7a576fe6486151aeae28104b414450c9579bd07e
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-05-22 17:23:56 +0300

    5. Loripsum

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const fs = require('fs');
+const request = require('request');
+const url = process.argv[2];
+const filePath = process.argv[3];
+request(url, 'utf-8', (err, res, data) => {
+  if (err) {
+    console.log(err);
+  }
+  if (data) {
+    fs.writeFile(filePath, data, 'utf-8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
